19aa47923557dbd08149634c3581656b0e330b1d
Anshika0208 <jainanshika2416@gmail.com>
2021-08-09 22:55:40 +0530

    Added app "Components" with reusable components(Apporval card for comment)

--- components/src/index.js
+++ components/src/index.js
@@ -1,11 +1,38 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import faker from "faker";
+import CommentDetails from "./CommentDetails";
+import ApprovalCard from "./ApprovalCard";
 
 const App = () => {
-  return <div>Hi there!</div>;
-}
+  return (
+    <div className="ui container comments">
+      <ApprovalCard>
+        <CommentDetails
+          author="Sam"
+          date="Today 4.15"
+          comment="Good job!"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetails
+          author="Alex"
+          date="Today 16.15"
+          comment="Good Work!"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetails
+          author="Jane"
+          date="Today 8.15"
+          comment="Nice"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+    </div>
+  );
+};
 
-ReactDOM.render(
-    <App />,
-  document.querySelector('#root')
-);
+ReactDOM.render(<App />, document.querySelector("#root"));
