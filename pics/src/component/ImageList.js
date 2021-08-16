9ddd7e85c0772cdcdaa72e38e4e62bd5fa77aee7
Anshika0208 <jainanshika2416@gmail.com>
2021-08-16 13:24:41 +0530

    Added grid and style to properly display images

--- pics/src/component/ImageList.js
+++ pics/src/component/ImageList.js
@@ -1,11 +1,14 @@
-import React from 'react';
+import "./ImageList.css";
+import ImageCard from "./ImageCard";
+import React from "react";
 
-
-const ImageList = props => {
-    const images = props.images.map(image => {
-        return <img key={image.id} src={image.urls.regular} alt={image.description}/>;
-    });
-    return <div>{images}</div>;
+const ImageList = (props) => {
+  const images = props.images.map((image) => {
+    return (
+      <ImageCard key={image.id} image={image}/>
+    );
+  });
+  return <div className="image-list">{images}</div>;
 };
 
 export default ImageList;
