e53b040f05b9e2bf7ef778b5f9e96dd344c6147b
Anshika0208 <32234443+Anshika0208@users.noreply.github.com>
2021-09-06 06:02:42 +0530

    Delete pics directory

--- pics/src/api/unsplash.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-export default axios.create({
-  baseURL: "https://api.unsplash.com",
-  headers: {
-    Authorization: "Client-ID 9qa4mSS7959Yv9bf18L8TkfKG2BAN5040omPjSgyejQ",
-  }
-});
