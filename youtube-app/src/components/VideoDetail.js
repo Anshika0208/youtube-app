bd754761970a4aa6898ef9402db8231a4e22af62
Anshika0208 <32234443+Anshika0208@users.noreply.github.com>
2021-09-06 06:08:51 +0530

    Update VideoDetail.js

--- youtube-app/src/components/VideoDetail.js
+++ youtube-app/src/components/VideoDetail.js
@@ -7,7 +7,7 @@ const VideoDetail = ({ video }) => {
   return (
     <div>
       <div className="ui embed">
-        <iframe src={videoSrc} />
+        <iframe title={videoSrc} src={videoSrc} />
       </div>
       <div className="ui segment">
         <h4 className="ui header">{video.snippet.title}</h4>
